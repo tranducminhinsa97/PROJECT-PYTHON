f2d9183010b018dcc7d4819860db0772e54fef03
tranducminhinsa97 <tranducminh.lhp97@gmail.com>
2018-02-01 02:16:16 +0100

    Update Fitness.py
    Update the method of counting fitness score for hands
    Update the method for checking the connections ( using classs,name instead of == list )
    Update the function does_clock_run

--- Fitness.py
+++ Fitness.py
@@ -9,32 +9,31 @@ import math
 import generation as gen
 import random
 
-# HAND VITESSE
-VITESSE_SECONDHAND = 1 / 60
-VITESSE_MINUTEHAND = 1 / 3600
-VITESSE_HOURHAND = 1 / 43200
+# HAND VITESSE (TOUR/Second)
+VITESSE_SECONDHAND = 1. / 60
+VITESSE_MINUTEHAND = 1. / 3600
+VITESSE_HOURHAND = 1. / 43200
 
 # POINTS FOR CONNECTIONS
 OBJECT_Z_AXIS = 0.01  
 OBJECT_PINNED_AXIS = 0.01
-GEAR_Z_GEAR = 0.02
-GEAR_XY_GEAR = 0.02
-GEAR_Z_HAND = 0.02
-REMONTOIRE_XY_ROCHET = 0.10
-SPRING_Z_BARREL = 0.010
-BARREL_XY_SPRING = 0.010
-FORK_XY_ESCAPE_WHEEL = 0.020
-SPRING_XY_FORK = 0.010
+GEAR_Z_GEAR = 0.03
+GEAR_XY_GEAR = 0.03
+GEAR_Z_HAND = 0.05
+SPRING_Z_BARREL = 0.020
+BARREL_XY_SPRING = 0.020
+FORK_XY_ESCAPE_WHEEL = 0.1
+BALANCEWHEEL_XY_FORK = 0.1
 BARREL_XY_GEAR = 0.010
-BARREL_Z_BARREL = 0.010
-SPRING_Z_BALANCEWHEEL = 0.010
-GEAR_XY_ESCAPE_WHEEL = 0.02
+SPRING_Z_BALANCEWHEEL = 0.050
+GEAR_XY_ESCAPE_WHEEL = 0.05
 # INITIAL VITESSE
-VITESSE_INITIAL = 2
+VITESSE_INITIAL = 1/60
 
 #NUMBER OF GENERATIONS:
-NUMBER_INDIVIDUALS = 10
 NUMBER_GENERATIONS = 1000
+NUMBER_CROSSING = 1000
+
 
 class Connection():
     """
@@ -45,7 +44,9 @@ class Connection():
         self.name = name
         self.point = point
 
+
 ConnectionData = []
+
 
 def input_database():
 	"""
@@ -53,24 +54,32 @@ def input_database():
 	"""
 	for thing in ["Gear", "Escape_Wheel", "Barrel", "Balance_Wheel", "Spring", "Hand", "Fork"]:
 		ConnectionData.append(Connection([thing, "Axis", gen.Z_CONNECTION], OBJECT_PINNED_AXIS))
+		ConnectionData.append(Connection(["Axis", thing, gen.Z_CONNECTION], OBJECT_PINNED_AXIS))
+
 	ConnectionData.append(Connection(["Gear", "Gear", gen.Z_CONNECTION], GEAR_Z_GEAR))
 	ConnectionData.append(Connection(["Gear", "Gear", gen.XY_CONNECTION], GEAR_XY_GEAR))
+
 	ConnectionData.append(Connection(["Gear", "Hand", gen.Z_CONNECTION], GEAR_Z_HAND))
 	ConnectionData.append(Connection(["Hand", "Gear", gen.Z_CONNECTION], GEAR_Z_HAND))
+
 	ConnectionData.append(Connection(["Fork", "EscapeWheel", gen.XY_CONNECTION], FORK_XY_ESCAPE_WHEEL))
 	ConnectionData.append(Connection(["EscapeWheel", "Fork", gen.XY_CONNECTION], FORK_XY_ESCAPE_WHEEL))
-	ConnectionData.append(Connection(["Spring","Fork", gen.XY_CONNECTION], SPRING_XY_FORK))
-	ConnectionData.append(Connection(["Fork", "Spring", gen.XY_CONNECTION], SPRING_XY_FORK))
+
+	ConnectionData.append(Connection(["BalanceWheel","Fork", gen.XY_CONNECTION], BALANCEWHEEL_XY_FORK))
+	ConnectionData.append(Connection(["Fork", "BalanceWheel", gen.XY_CONNECTION], BALANCEWHEEL_XY_FORK))
+
 	ConnectionData.append(Connection(["EscapeWheel", "Gear", gen.XY_CONNECTION],GEAR_XY_ESCAPE_WHEEL))
 	ConnectionData.append(Connection(["Gear", "EscapeWheel", gen.XY_CONNECTION], GEAR_XY_ESCAPE_WHEEL))
+
 	# ConnectionData.append(Connection(["Remontoir","Rochet",HORIZONTAL],REMONTOIRE_XY_ROCHET))
 	ConnectionData.append(Connection(["Spring", "Barrel", gen.Z_CONNECTION], SPRING_Z_BARREL))
 	ConnectionData.append(Connection(["Barrel", "Spring", gen.Z_CONNECTION], SPRING_Z_BARREL))
+
 	ConnectionData.append(Connection(["Spring", "BalanceWheel", gen.Z_CONNECTION], SPRING_Z_BALANCEWHEEL))
 	ConnectionData.append(Connection(["BalanceWheel", "Spring", gen.Z_CONNECTION], SPRING_Z_BALANCEWHEEL))
+
 	ConnectionData.append(Connection(["Barrel", "Gear", gen.XY_CONNECTION], BARREL_XY_GEAR))
 	ConnectionData.append(Connection(["Gear", "Barrel", gen.XY_CONNECTION], BARREL_XY_GEAR))
-	ConnectionData.append(Connection(["Barrel", "Barrel", gen.Z_CONNECTION], BARREL_Z_BARREL))
 
 
 def is_pinned(obj1, connection_list):
@@ -118,25 +127,30 @@ def does_clock_runs(connection_list):
 	#INPUT: A LIST OF CONNECTION OF A CLOCK
     # Verify if a clock actually runs or not. Initial a vitesse for escape wheel
 
-    if ((["Spring", "BalanceWheel", gen.Z_CONNECTION] in connection_list) and (
-                ["Spring","Fork", gen.XY_CONNECTION] in connection_list) and (
-                ["Fork", "EscapeWheel", gen.XY_CONNECTION] in connection_list) and (
-                ["EscapeWheel", "Gear", gen.XY_CONNECTION] in connection_list) and (
-                ["Gear", "Barrel", gen.XY_CONNECTION] in connection_list) and (
-                ["Gear", "Hand", gen.Z_CONNECTION] in connection_list)and (
-                ["Spring", "Barrel", gen.Z_CONNECTION] in connection_list)):
-        return True
-    return False
-def initial_clock_runs(clock):
+	if (([gen.Spring, gen.BalanceWheel, gen.Z_CONNECTION] in connection_list) and (
+                [gen.BalanceWheel,gen.Fork, gen.XY_CONNECTION] in connection_list) and (
+                [gen.Fork, gen.EscapeWheel, gen.XY_CONNECTION] in connection_list) and (
+                [gen.EscapeWheel, gen.Gear, gen.XY_CONNECTION] in connection_list) and (
+                [gen.Gear, gen.Barrel, gen.XY_CONNECTION] in connection_list) and (
+                [gen.Gear, gen.Hand, gen.Z_CONNECTION] in connection_list)and (
+                [gen.Spring, gen.Barrel, gen.Z_CONNECTION] in connection_list) and (
+                [gen.Gear, gen.Gear, gen.Z_CONNECTION] in connection_list) and(
+                [gen.Gear, gen.Gear, gen.XY_CONNECTION] in connection_list)):
+		return True
+	return False
+
+
+def initial_clock_run(clock):
 	#INPUT : LIST OF OBJECTS OF A CLOCK
 	#OUTPUT : IF THE CLOCK RUNS< INITIALIZE THE MOVEMENT OF ESCAPE WHEEL.
 	connection_list = gen.list_connections(clock)
-	if does_clock_runs(connection_list):
+	if does_clock_run(connection_list):
 		for i in range(1, len(clock)):
 			if clock[i].__class__.__name__ == "EscapeWheel":
 				clock[i].speed = VITESSE_INITIAL
 
-def i_in_connectdata(connection1):
+
+def is_in_connectdata(connection1):
 	#INPUT : A list type [object, object, type of connection]
 	#OUTPUT : A Point correspond with the connection in ConnectionData, 0 if the input connection doesn't appear in ConnectionData
 	for i in range(len(ConnectionData)):
@@ -155,21 +169,25 @@ def fitness(clock):
 	connection_list = gen.list_connections(clock)
 	score = 0
 	for cn in connection_list:
-		if (i_in_connectdata(cn)!=0):
-			score += i_in_connectdata(cn)
+		if (is_in_connectdata(cn)!=0):
+			score += is_in_connectdata(cn)
 
 	# EVALUATE THE CLOCK WHEN IT RUNS
-	if (does_clock_runs(connection_list)):
+	if (does_clock_run(connection_list)):
 		initial_clock_runs(clock)
+		# AWARD FOR RUNNING CLOCK
+		score += 100.0 ;
+		# CHECK FOR EACH CONNECTION OF RUNNING CLOCK
 		for cn in connection_list:
 			print("Tick Tock Tick Tock Tick Tock")
-			if ((cn == [gen.Gear, gen.Hand, gen.Z_CONNECTION]) or (cn == [gen.Hand, gen.Gear, gen.Z_CONNECTION]) or (
-				cn == [gen.Gear, gen.Gear, gen.Z_CONNECTION])):
-				# IF A GEAR AND A HAND CONNECT VERTICALLY THAN THE HAND WILL SPIN
-				objects_Z_connection(cn[0], cn[1])
-			if (cn == (gen.Gear, gen.Gear, gen.XY_CONNECTION)):
+			x = [cn[0].__class__.__name__,cn[1].__class__.__name__,cn[2]]
+			if ((x[0]=="Hand") and (x[1]=="Gear") and (x[2]==XY_CONNECTION)):
 				# IF 2 GEARS CONNECT HORIZONTALLY THEN THE OTHER GEAR WILL SPIN
 				gears_XY_connection(cn[0], cn[1])
+			if ((x[0]=="Hand") and (x[1]=="Gear") and (x[2]==Z_CONNECTION)) or ((x[1]=="Hand") and (x[0]=="Gear") and (x[2]==Z_CONNECTION)) or ((x[0]=="Gear") and (x[1]=="Gear") and (x[2]==Z_CONNECTION)):
+				# IF A GEAR AND A HAND CONNECT VERTICALLY THAN THE HAND WILL SPIN
+				objects_Z_connection(cn[0], cn[1])
+			
 			if (cn[1] == gen.Hand):
 				hand = cn[1]
 			if (cn[0] == gen.Hand):
@@ -177,15 +195,20 @@ def fitness(clock):
 			"""
 			EVALUATE CLOCK
 			"""
-			SEC_SCORE = 1 - abs((abs(hand.speed) - VITESSE_SECONDHAND) / VITESSE_SECONDHAND)
-			MIN_SCORE = 1 - abs((abs(hand.speed) - VITESSE_MINUTEHAND) / VITESSE_MINUTEHAND)
-			HOU_SCORE = 1 - abs((abs(hand.speed) - VITESSE_HOURHAND) / VITESSE_HOURHAND)
-			if SEC_SCORE == max(SEC_SCORE, MIN_SCORE, HOU_SCORE):
+			if (hand.speed < 1./30) and (hand.speed > 1./90): 
+				# IN THIS CASE THE HAND MAYBE THE SECOND HAND, A ROUND WILL TAKE BETWEEN 30s AND 90S
+				SEC_SCORE = 1 - abs((abs(hand.speed) - VITESSE_SECONDHAND) / VITESSE_SECONDHAND)
 				SCORE_HAND = SEC_SCORE
-			elif MIN_SCORE == max(SEC_SCORE, MIN_SCORE, HOU_SCORE):
+			elif (hand.speed < 1./1800) and (hand.speed > 1./5400):
+				# IN THIS CASE THE HAND MAYBE THE MINUTE HAND, A ROUND WILL TAKE BETWEEN 1800s( 30 minutes) to 5400s (90 minutes).
+				MIN_SCORE = 1 - abs((abs(hand.speed) - VITESSE_MINUTEHAND) / VITESSE_MINUTEHAND)
 				SCORE_HAND = 60 * MIN_SCORE
-			else:
+			elif (hand.speed < 1./21600) and (hand.speed > 1./64800):
+				# IN THIS CASE THE HAND MAYBE THE HOUR HAND, A ROUND WILL TAKE BETWEEN 21600S (12H) TO 64800(36H).
+				HOU_SCORE = 1 - abs((abs(hand.speed) - VITESSE_HOURHAND) / VITESSE_HOURHAND)
 				SCORE_HAND = 3600 * HOU_SCORE
+			else : 
+				SCORE_HAND = 0
 			score += SCORE_HAND
 	return round(score,3)
 
@@ -206,8 +229,10 @@ def fitness_average(population1):
 	for clock in population1:
 		sum_fitness += fitness(clock)
 	return sum_fitness/len(population1)
+
+
 def crossing(population1):
-	for i in range(0,100):
+	for i in range(0,NUMBER_CROSSING):
 		obj1 = population1.pop(random.randrange(len(population1)))
 		obj2 = population1.pop(random.randrange(len(population1)))
 		obj3 = population1.pop(random.randrange(len(population1)))
@@ -238,7 +263,7 @@ def crossing(population1):
 		population1.append(dad_clock)
 		population1.append(mom_clock)
 		population1.append(baby_clock)
-
+		#population1.append(destroyed_clock)
 		
 
 def natural_selection(population1):
@@ -246,13 +271,15 @@ def natural_selection(population1):
 		crossing(population1)
 		print("Generation",j)
 		print("AVERAGE SCORE: ",fitness_average(population1))
-		#for clock in population1:
-		#	if fitness(clock) <= fitness_average(population1):
-		#		population1.remove(clock)
+		"""
+		for clock in population1:
+			if fitness(clock) <= fitness_average(population1):
+				population1.remove(clock)
+		"""
 		print(fitness_population(population1))		
 		best_cl = best_clock(population1)
 		print("BEST CLOCK FITNESS SCORE:",fitness(best_cl))
-		#display_clock(best_cl)
+		display_clock(best_cl)
 		
 def best_clock(population1):
 	#RETURN THE BEST CLOCK
@@ -279,3 +306,8 @@ natural_selection(population)
 #best = best_clock(population)
 #print("BEST CLOCK FITNESS SCORE:",fitness(best))
 
+for i in range(len(ConnectionData)) :
+	print(i, " : ", ConnectionData[i].name, " -> ", ConnectionData[i].point)
+print( "   ",1 - abs((abs(1./60) - VITESSE_SECONDHAND) / VITESSE_SECONDHAND))
+print( "   ",1 - abs((abs(1/.60) - VITESSE_MINUTEHAND) / VITESSE_MINUTEHAND))
+print( "   ",1 - abs((abs(1./60) - VITESSE_HOURHAND) / VITESSE_HOURHAND))
